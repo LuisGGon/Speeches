4bebd08a325d62e01d54ca4471dc399a25365d07
Luis Giraldo <luis.giraldo.gonzalez@gmail.com>
2021-07-09 14:02:39 +0200

    I found the way to save the date of one discourse in a .json file.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # import libraries
 import datetime
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,17 +16,28 @@ page = requests.get(quote_page)
 # parse the html using beautiful soup and store in variable `soup`
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# finding the date of the speech
-date_box = soup.find('span', attrs={'class': 'date-display-single'})
-
-date = date_box.text.strip()  # strip() is used to remove starting and trailing
-date_d = datetime.datetime.strptime(date, '%d/%m/%Y').date()
-
 # finding the title of the speech
 title_box = soup.find('h1', attrs={'class': 'node-title'})
-
 title = title_box.text.strip()
-print(title)
 
-discourse = {'Date': date, 'Title': title}
-print(discourse)
+# Here I recover the text of the speech and more stuff
+speech = []
+for blabla in soup.find_all('div', attrs={'class': 'field-item even'}):
+    speech.append(blabla.text.strip())
+print(speech)
+
+# To recover the date
+date = speech[0]
+text = speech[1]
+
+# Here I create a Python dictionary to store the information from the speech
+discourse = {'Date': date, 'Title': title, 'Text': text}
+
+with open('index.json', 'w') as ID:
+    json.dump(discourse, ID)
+
+# finding the date of the speech
+# date_box = soup.find('span', attrs={'class': 'date-display-single'})
+
+# date = date_box.text.strip()  # strip() is used to remove starting and trailing
+# date_d = datetime.datetime.strptime(date, '%d/%m/%Y').date()
